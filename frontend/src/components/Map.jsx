2280cdf1a101dc7af929ea3eec571cf6045ae93d
Fx-Colliot <fxcolliot@gmail.com>
2021-04-25 17:06:51 +0200

    Map ok without circle

--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -1,43 +1,84 @@
-import React, { useEffect, useState } from "react";
-import L from "leaflet";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import React, { useEffect, useRef, useState } from "react";
+import { Circle, MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import { MapDiv } from "../styledComponents/MapStyled";
 import axios from "axios";
+import geoLocation from "../GeoLocation/geoLocation";
 
 function Map() {
-  const [locations, setLocations] = useState([]);
-  //const [activeSpace, setActiveSpace] = useState(null);
+  const mapRef = useRef();
+  const [spaces, setSpaces] = useState([]);
+  const location = geoLocation();
+
+  //const showMyLocation = () => {
+  //  if (location.loaded && !location.error) {
+  //    flyTo([location.coordinates.lat, location.coordinates.lng]);
+  //  } else {
+  //    alert("error");
+  //  }
+  //};
+
+  //Location navigator ok, not work with button flyTo
+  //Plus de carte avec le circle
+  //Peut etre changer la géolocalisation, react geolocated
+  //Changer tilelayer
 
   useEffect(() => {
     axios.get("http://localhost:5050/places").then(({ data }) => {
-      setLocations(data);
+      setSpaces(data);
     });
   }, []);
-  console.log(locations);
+  console.log(spaces);
+
   return (
     <MapDiv>
       <MapContainer
         center={[50.62925, 3.057256]}
         zoom={13}
-        minZoom={13}
-        maxZoom={17}
+        //minZoom={12}
+        //maxZoom={17}
       >
         <TileLayer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png" />
-        {locations.map((location) => (
+
+        {location.loaded && !location.error && (
+          <>
+            <Marker
+              position={[location.coordinates.lat, location.coordinates.lng]}
+            >
+              <Circle
+                center={[location.coordinates.lat, location.coordinates.lng]}
+                radius={300}
+                color="green"
+                fillColor="#0163FF"
+                fillOpacity={0.4}
+                opacity={1}
+              />
+
+              <Popup>
+                <p>toto</p>
+              </Popup>
+            </Marker>
+          </>
+        )}
+        {/*
+        {spaces.map((space) => (
           <Marker
-            key={location.idplaces}
-            position={[location.latitude, location.longitude]}
+            key={space.idplaces}
+            position={[space.latitude, space.longitude]}
             onClick={() => {}}
           >
-            <Popup position={[location.latitude, location.longitude]}>
+            <Popup position={[space.latitude, space.longitude]}>
               <div>
-                <h3>{location.adresse}</h3>
+                <h3>{space.adresse}</h3>
               </div>
             </Popup>
             /
           </Marker>
-        ))}
+        ))}*/}
       </MapContainer>
+
+      {/*} <div>
+        <button onClick={showMyLocation}>Locate Me</button>
+      </div>*/}
     </MapDiv>
   );
 }
